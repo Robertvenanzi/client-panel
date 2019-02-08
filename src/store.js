145a1a6ed63b522746f642fa6f3e16eb77451e1f
robertvenanzi <robertgvenanzi@gmail.com>
2019-02-07 19:54:01 -0800

    declared firestore

--- src/store.js
+++ src/store.js
@@ -24,7 +24,8 @@ const rrfConfig = {
 // Init firebase instance
 firebase.initializeApp(firebaseConfig);
 // Init firestore
-// const firestore = firebase.firestore();
+const firestore = firebase.firestore();
+
 
 // Add reactReduxFirebase enhancer when making store creator
 const createStoreWithFirebase = compose(
