b6aaf67ab6ed69706eeb3148359915d3be65d8b8
robertvenanzi <robertgvenanzi@gmail.com>
2019-02-07 19:56:09 -0800

    brought in clients and spinner

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -4,11 +4,28 @@ import PropTypes from 'prop-types';
 import { compose } from 'redux';
 import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
-
+import Spinner from '../layout/Spinner';
 
 class Clients extends Component {
+    state = {
+        totalOwed: null
+    };
+
+    static getDerivedStateFromProps(props, state) {
+        const { clients } = props;
+
+        if (clients) {
+            const total = clients.reduce((total, client) => {
+                return total + parseFloat(client.balance.toString());
+            }, 0);
+            return { totalOwed: total }
+        }
+        return null;
+    };
+
     render() {
         const { clients } = this.props;
+        const { totalOwed } = this.state;
 
         if (clients) {
             return (
@@ -18,7 +35,10 @@ class Clients extends Component {
                             <h2><i className="fas fa-users"></i> Clients</h2>
                         </div>
                         <div className="col-md-6">
-
+                            <h5 className="text-right text-scondary">Total Owed {' '}
+                                <span className="text-primary">
+                                    ${parseFloat(totalOwed).toFixed(2)}
+                                </span></h5>
                         </div>
                     </div>
                     <table className="table table-striped">
@@ -48,7 +68,7 @@ class Clients extends Component {
                 </div>
             )
         } else {
-            return <h1>Loading..</h1>
+            return <Spinner />
         }
 
     }
@@ -62,6 +82,6 @@ Clients.propTypes = {
 export default compose(
     firestoreConnect([{ collection: 'clients' }]),
     connect((state, props) => ({
-        clients: state.firebase.ordered.clients
+        clients: state.firestore.ordered.clients
     }))
 )(Clients);
