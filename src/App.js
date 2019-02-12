4a8724e84423e080c547ffefbfdfea1970048261
robertvenanzi <robertgvenanzi@gmail.com>
2019-02-11 19:22:18 -0800

    imported EditClient and Created a Route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import store from './store';
 import Dashboard from './components/layout/Dashboard';
 import AppNavbar from './components/layout/AppNavbar';
 import AddClient from './components/clients/AddClient';
+import EditClient from './components/clients/EditClient';
 import ClientDetails from './components/clients/ClientDetails';
 
 
@@ -23,6 +24,7 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
                 <Route exact path="/client/:id" component={ClientDetails} />
 
               </Switch>
